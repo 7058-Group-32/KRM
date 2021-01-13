5570d3b3bd9c44fafb8a628a43519ce3f476ef13
Matthew7058 <matthewfard@me.com>
2021-01-13 22:44:07 +0000

    In progress of creating registration form: Working on Controller

--- register.php
+++ register.php
@@ -1,5 +1,42 @@
 <?php
+require_once('Models/UserDataSet.php');
 $view = new stdClass();
 $view->pageTitle = 'Homepage';
+
+session_start();
+$userData = new UserDataSet();
+
+if(isset($_POST['register']))
+{
+    $name = htmlentities($_POST['name']);
+    $email = htmlentities($_POST['email']);
+    $number = htmlentities($_POST['number']);
+    $address1 = htmlentities($_POST['address1']);
+    $address2 = htmlentities($_POST['address2']);
+    $postcode = htmlentities($_POST['postcode']);
+    $password1 = htmlentities($_POST['password1']);
+    $password2 = htmlentities($_POST['password2']);
+
+    $address = $address1 . ', ' . $address2 . ', ' . $postcode;
+
+    $password = null;
+    $encryptedPass = null;
+    if ($password1 = $password2)
+    {
+        $password = $password1;
+        $encryptedPass= password_hash($password,PASSWORD_DEFAULT);
+    }
+
+    $_SESSION["registered"] = false;
+
+    if($password != null)
+    {
+        $userData->register($name, $email, $number, $address, $encryptedPass);
+        $_SESSION["registered"] = true;
+    }
+
+
+}
+
 require_once('Views/register.phtml');
 ?>
