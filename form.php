65b519e1e0f722e58984139e603580542a2b1f52
Lucas Whiteley <77293959+lucaswhiteley@users.noreply.github.com>
2021-01-14 14:10:18 +0000

    Added the form code
    Added the code for the form

--- form.php
+++ form.php
@@ -2,4 +2,45 @@
 $view = new stdClass();
 $view->pageTitle = 'Application';
 require_once('Views/form.phtml');
-?>
+
+if(!isset($_POST['project-name'])){$_POST['project-name'] = '';}
+if(!isset($_POST['customer-name'])){$_POST['customer-name'] = '';}
+if(!isset($_POST['deadline'])){$_POST['deadline'] = '';}
+if(!isset($_POST['email'])){$_POST['email'] = '';}
+if(!isset($_POST['budget-range'])){$_POST['budget-range'] = '';}
+if(!isset($_POST['short-description'])){$_POST['short-description'] = '';}
+
+$projectName = $_POST['project-name'];
+$customerName = $_POST['customer-name'];
+$deadline = $_POST['deadline'];
+$email = $_POST['email'];
+$budgetRange = $_POST['budget-range'];
+$shortDescription = $_POST['short-description'];
+
+
+//if all of the information has been set, then you can send the mail
+if (isset($_POST['submit']) || $_POST['project-name'] || $_POST['customer-name'] || $_POST['deadline'] || $_POST['email'] || $_POST['budget-range'] || $_POST['short-description'])
+{
+
+
+    //Recipient of the email
+    $to = 'l.whiteley1@edu.salford.ac.uk';
+
+    //Subject of the email
+    $subject = $projectName;
+
+    //The body of the message
+    $message = '<p><b>'. $projectName .'</b></p> <p>'.$customerName.'</p> <p>'. $deadline . '</p> <p>'. $budgetRange .'<p>'. $shortDescription .'</p>';
+
+    //Headers of the message
+    $headers = "From: " . $customerName . "<".$email.">\r\n";
+    $headers .=  "Reply-To: " . $customerName . "<".$email.">\r\n";
+    $headers .="Content-type: text/html\r\n";
+
+    //Sends the mail
+    mail($to,$subject,$message, $headers);
+
+
+}else{
+    echo 'Not all fields are complete';
+}
