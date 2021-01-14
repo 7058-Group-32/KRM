eb3dcc818ce022acaec7b6b01896412c0d05a35b
Lucas Whiteley <77293959+lucaswhiteley@users.noreply.github.com>
2021-01-14 15:04:06 +0000

    Added function - addApplication
    Added a function to add an application to the database

--- Models/ApplicationDataSet.php
+++ Models/ApplicationDataSet.php
@@ -11,4 +11,14 @@ class ApplicationDataSet
         $this->_dbInstance = Database::getInstance();
         $this->_dbHandle = $this->_dbInstance->getdbConnection();
     }
+    
+    //Adds an application to the database
+     public function addApplication($projectName, $customerName, $description, $minRange, $maxRange, $deadline, $otherReq, $userID)
+    {
+        $sqlQuery = "INSERT INTO Users(ProjectName, CustomerName, Description, MinRange, MaxRange, Deadline, OtherRequirements, UserID)
+        VALUES ('" . $projectName . "','" . $customerName . "','" . $description . "','" . $minRange . "'. $maxRange . "','" . $deadline . "','" . $otherReq . "','" . $userID . "')";
+        $statement = $this->_dbHandle->prepare($sqlQuery);
+        $statement->execute();
+    }
+    
 }
