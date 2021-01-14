f67ba3adecacbff846c1680ce5deaf793d658b13
Lucas Whiteley <77293959+lucaswhiteley@users.noreply.github.com>
2021-01-14 15:30:37 +0000

    Fix to add Application

--- Models/ApplicationDataSet.php
+++ Models/ApplicationDataSet.php
@@ -15,8 +15,8 @@ class ApplicationDataSet
     //Adds an application to the database
      public function addApplication($projectName, $customerName, $description, $minRange, $maxRange, $deadline, $otherReq, $userID)
     {
-        $sqlQuery = "INSERT INTO Users(ProjectName, CustomerName, Description, MinRange, MaxRange, Deadline, OtherRequirements, UserID)
-        VALUES ('" . $projectName . "','" . $customerName . "','" . $description . "','" . $minRange . "'. $maxRange . "','" . $deadline . "','" . $otherReq . "','" . $userID . "')";
+         
+        $sqlQuery = "INSERT INTO Application(ProjectName, CustomerName, Description, MinRange, MaxRange, Deadline, OtherRequirements, UserID) VALUES ('" . $projectName . "','" . $customerName . "','" . $description . "','" . $minRange . "','" . $maxRange . "','" . $deadline . "','" . $otherReq . "','" . $userID . "')";
         $statement = $this->_dbHandle->prepare($sqlQuery);
         $statement->execute();
     }
