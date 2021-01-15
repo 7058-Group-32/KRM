e8e75329fc0b18953cdfb90075ce3d92911faa92
Lucas Whiteley <77293959+lucaswhiteley@users.noreply.github.com>
2021-01-15 12:20:23 +0000

    Changed budget range to min and max budget range

--- form.php
+++ form.php
@@ -11,7 +11,8 @@ if(!isset($_POST['project-name'])){$_POST['project-name'] = '';}
 if(!isset($_POST['customer-name'])){$_POST['customer-name'] = '';}
 if(!isset($_POST['deadline'])){$_POST['deadline'] = '';}
 if(!isset($_POST['email'])){$_POST['email'] = '';}
-if(!isset($_POST['budget-range'])){$_POST['budget-range'] = '';}
+if(!isset($_POST['min-budget-range'])){$_POST['min-budget-range'] = '';}
+if(!isset($_POST['max-budget-range'])){$_POST['max-budget-range'] = '';}
 if(!isset($_POST['short-description'])){$_POST['short-description'] = '';}
 if(!isset($_POST['other-requirement'])){$_POST['other-requirement'] = '';}
 
@@ -19,12 +20,13 @@ $projectName = $_POST['project-name'];
 $customerName = $_POST['customer-name'];
 $deadline = $_POST['deadline'];
 $email = $_POST['email'];
-$budgetRange = $_POST['budget-range'];
 $shortDescription = $_POST['short-description'];
 $otherReq = $_POST['other-requirement'];
+$minBudgetRange = $_POST['min-budget-range'];
+$maxBudgetRange = $_POST['max-budget-range'];
 
 //if all of the information has been set, then you can send the mail
-if (isset($_POST['submit']) || $_POST['project-name'] || $_POST['customer-name'] || $_POST['deadline'] || $_POST['email'] || $_POST['budget-range'] || $_POST['short-description'])
+if (isset($_POST['submit']) || $_POST['project-name'] || $_POST['customer-name'] || $_POST['deadline'] || $_POST['email'] || $_POST['min-budget-range'] || $_POST['max-budget-range'] || $_POST['short-description'])
 {
 
 
@@ -35,7 +37,7 @@ if (isset($_POST['submit']) || $_POST['project-name'] || $_POST['customer-name']
     $subject = $projectName;
 
     //The body of the message
-    $message = '<p><b>'. $projectName .'</b></p> <p>'.$customerName.'</p> <p>'. $deadline . '</p> <p>'. $budgetRange .'<p>'. $shortDescription .'</p>' . $otherReq . '</p>';
+    $message = '<p><b>'. $projectName .'</b></p> <p>'.$customerName.'</p> <p>'. $deadline . '</p> <p>'. $minBudgetRange . '</p> <p>'. $maxBudgetRange .'<p>'. $shortDescription .'</p>' . $otherReq . '</p>';
 
     //Headers of the message
     $headers = "From: " . $customerName . "<".$email.">\r\n";
@@ -47,7 +49,7 @@ if (isset($_POST['submit']) || $_POST['project-name'] || $_POST['customer-name']
     
     //min and max have been given the placeholder value of budgetRange
     //no way to get userID currently
-    //addApplication($projectName, $customerName, $shortDescription, $budgetRange, $budgetRange, $deadline, $otherReq, $userID);
+    //addApplication($projectName, $customerName, $shortDescription, $minBudgetRange, $maxBudgetRange, $deadline, $otherReq, $userID);
 
 }else{
     echo 'Not all fields are complete';
