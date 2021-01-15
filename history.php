fd445e6b37f1561c55aad64dca35e720b6f2ca57
Matthew7058 <matthewfard@me.com>
2021-01-15 21:51:31 +0000

    Making use of fetchUserApplications method in history.php

--- history.php
+++ history.php
@@ -4,7 +4,12 @@ $view = new stdClass();
 $view->pageTitle = 'History';
 
 session_start();
+$applicationData = new applicationDataSet();
 
+if (isset($_SESSION["loggedin"]))
+{
+    $view->applicationData = $applicationData->fetchUserApplications($_SESSION["userid"]);
+}
 
 
 require_once('Views/history.phtml');
