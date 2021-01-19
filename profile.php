e216363f7d0f356a64e981fcdb7ab5ac20b57243
Matthew7058 <matthewfard@me.com>
2021-01-19 20:09:50 +0000

    Implementing profile page. Adding php

--- profile.php
+++ profile.php
@@ -21,8 +21,30 @@ if(isset($_POST['delete']))
 
 if(isset($_POST['save']))
 {
-    $userData->deleteUser($id);
-    $_SESSION["deleted"] = true;
+
+    $newName = htmlentities($_POST['userInfo-name']);
+    $newEmail = htmlentities($_POST['userInfo-email']);
+    $newNumber = htmlentities($_POST['userInfo-phone']);
+    $newAddress1 = htmlentities($_POST['userInfo-ad1']);
+    $newAddress2 = htmlentities($_POST['userInfo-ad2']);
+    $newPostcode = htmlentities($_POST['userInfo-postcode']);
+    $oldPassword = htmlentities($_POST['userInfo-old-pass']);
+    $newPassword1 = htmlentities($_POST['userInfo-pass1']);
+    $newPassword2 = htmlentities($_POST['userInfo-pass2']);
+
+    if ($newName != null)
+    {
+        $userData->changeName($id, $newName);
+        $_SESSION["na"] = $newName;
+    }
+
+    if ($newEmail != null)
+    {
+        $userData->changeEmail($id, $newEmail);
+        $_SESSION["em"] = $newEmail;
+    }
+
+    $_SESSION["saved"] = true;
 }
 
 
